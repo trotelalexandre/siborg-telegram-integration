42190fc83fa41303732bc98c188ecfa4daf56d9a
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 16:31:29 +0200

    fix: start

--- src/menus/onboarding.ts
+++ src/menus/onboarding.ts
@@ -4,7 +4,22 @@ import { BASE_URL, CHAIN_ID } from "../env";
 
 export const onboardingMenu = new Menu("onboarding-menu")
   .text("Start to display ads", async (ctx) => {
-    await ctx.reply("");
+    await ctx.reply(
+      "To start displaying ads, you need to enter the following commands:"
+    );
+    await ctx.reply("/setup [offerId] [frequency]");
+    await ctx.reply(
+      "Example: /setup 1 5 (This will display ads from offer id 1 every 5 minutes)"
+    );
+    await ctx.reply(
+      "If you don't have an offer yet, you can create one by clicking the button below"
+    );
+    await ctx.reply("Create a new offer", {
+      reply_markup: new InlineKeyboard().url(
+        "Create a new offer",
+        `${BASE_URL}/${CHAIN_ID}/offer/create`
+      ),
+    });
   })
   .row()
   .text("Manage your offer and your tokens", async (ctx) => {
