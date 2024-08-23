11a20ce134fb01bb828e3353995719bc964e6dbf
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 12:34:52 +0200

    fix: bot token

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,8 @@
 import { Bot } from "grammy";
 import { TELEGRAM_BOT_TOKEN } from "./env";
 
+if (!TELEGRAM_BOT_TOKEN) {
+  throw new Error("TELEGRAM_BOT_TOKEN is not defined");
+}
+
 export const bot = new Bot(TELEGRAM_BOT_TOKEN);
