b0f799860a640c4b17081256470e9507fc78f671
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 12:57:17 +0200

    fix: bot logic

--- api/main.ts
+++ api/main.ts
@@ -1,5 +1,4 @@
-import { webhookCallback } from "grammy";
-import { bot } from "../src/bot";
+import { Bot, webhookCallback } from "grammy";
 import { businessCommand } from "../src/commands/business";
 import { helpCommand } from "../src/commands/help";
 import { manageCommand } from "../src/commands/manage";
@@ -8,8 +7,14 @@ import { startCommand } from "../src/commands/start";
 import { suggestCommands } from "../src/commands/suggest/commands";
 import { displayAdCommand } from "../src/commands/test/displayAd";
 import { fetchAdsCommand } from "../src/commands/test/fetchAds";
-import { CHAIN_ID } from "../src/env";
+import { CHAIN_ID, TELEGRAM_BOT_TOKEN } from "../src/env";
 import { messageFallbackHandler } from "../src/handlers/messageFallback";
+
+if (!TELEGRAM_BOT_TOKEN) {
+  throw new Error("TELEGRAM_BOT_TOKEN is not defined");
+}
+
+export const bot = new Bot(TELEGRAM_BOT_TOKEN);
 
 //export default webhookCallback(bot, "std/http");
 
--- src/bot.ts
+++
@@ -1,8 +0,0 @@
-import { Bot } from "grammy";
-import { TELEGRAM_BOT_TOKEN } from "./env";
-
-if (!TELEGRAM_BOT_TOKEN) {
-  throw new Error("TELEGRAM_BOT_TOKEN is not defined");
-}
-
-export const bot = new Bot(TELEGRAM_BOT_TOKEN);
