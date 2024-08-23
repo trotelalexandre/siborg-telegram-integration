b69083afb76b2fff606948daaaabe770d7a4847a
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 14:54:38 +0200

    fix: test mode

--- src/commands/test/testMode.ts
+++ src/commands/test/testMode.ts
@@ -1,10 +1,20 @@
 import type { Api, Bot, Context, RawApi } from "grammy";
-import { TEST_MODE_ENABLED } from "../../env";
+import { TEST_ENV, TEST_MODE_ENABLED } from "../../env";
 
 export const testModeCommand = (bot: Bot<Context, Api<RawApi>>) => {
   if (TEST_MODE_ENABLED) {
     bot.command("testmode", async (ctx) => {
       await ctx.reply("Test mode is enabled.");
     });
+    return;
+  }
+
+  if (TEST_ENV) {
+    bot.command("testmode", async (ctx) => {
+      await ctx.reply(
+        `Test mode is not enabled. TEST_ENV is set to ${TEST_ENV}`
+      );
+    });
+    return;
   }
 };
