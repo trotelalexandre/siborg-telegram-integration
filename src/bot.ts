a6f83b608f62948b28ce43986426f511fd6c605d
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 11:21:45 +0200

    change config

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,6 @@
-import { Bot } from "grammy";
+import { Bot, webhookCallback } from "grammy";
 import { TELEGRAM_BOT_TOKEN } from "./env";
 
 export const bot = new Bot(TELEGRAM_BOT_TOKEN);
+
+export default webhookCallback(bot, "std/http");
