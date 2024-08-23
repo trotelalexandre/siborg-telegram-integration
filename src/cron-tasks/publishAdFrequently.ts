970c87e3046caf3989535f781f750be633937f12
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 14:59:16 +0200

    fix: scheduler

--- src/cron-tasks/publishAdFrequently.ts
+++ src/cron-tasks/publishAdFrequently.ts
@@ -2,13 +2,31 @@ import cron from "node-cron";
 import fetchAd from "../utils/fetchAd";
 import { BASE_URL, CHAIN_ID } from "../env";
 import type { CommandContext, Context } from "grammy";
+import { TEST_MODE_ENABLED } from "../env";
 
 export const publishAdFrequentlyTask = async (
   ctx: CommandContext<Context>,
   frequency: number,
   offerId: number
 ) => {
+  let nextRunTime = new Date();
+
+  const updateNextRunTime = () => {
+    nextRunTime = new Date();
+    nextRunTime.setMinutes(nextRunTime.getMinutes() + frequency);
+  };
+
+  const getTimeLeft = () => {
+    const now = new Date();
+    return Math.max(
+      0,
+      Math.round((nextRunTime.getTime() - now.getTime()) / 1000)
+    );
+  };
+
   cron.schedule(`*/${frequency} * * * *`, async () => {
+    updateNextRunTime();
+
     const ad = await fetchAd(offerId).catch(async (error) => {
       await ctx.reply("Error fetching ad.");
     });
@@ -27,6 +45,15 @@ export const publishAdFrequentlyTask = async (
 
     console.log(`Ad fetched and displayed on the channel.`);
   });
+
+  if (TEST_MODE_ENABLED) {
+    cron.schedule("*/5 * * * * *", () => {
+      const timeLeft = getTimeLeft();
+      console.log(`Time left for the next ad fetch: ${timeLeft} seconds`);
+    });
+  }
+
+  updateNextRunTime();
 
   await ctx.reply(
     `Ad setup complete. Ads will be fetched every ${frequency} minutes.`
