c462d3040d9f7c44bdc57ded799a4e8e1511c197
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 13:55:53 +0200

    fix: init menus

--- api/main.ts
+++ api/main.ts
@@ -9,6 +9,7 @@ import { displayAdCommand } from "../src/commands/test/displayAd";
 import { fetchAdsCommand } from "../src/commands/test/fetchAds";
 import { CHAIN_ID, TELEGRAM_BOT_TOKEN } from "../src/env";
 import { messageFallbackHandler } from "../src/handlers/messageFallback";
+import { initMenus } from "../src/menus/init/initMenus";
 
 if (!TELEGRAM_BOT_TOKEN) {
   throw new Error("TELEGRAM_BOT_TOKEN is not defined");
@@ -17,6 +18,9 @@ if (!TELEGRAM_BOT_TOKEN) {
 export const bot = new Bot(TELEGRAM_BOT_TOKEN);
 
 //export default webhookCallback(bot, "std/http");
+
+// init menus
+initMenus(bot);
 
 // commands
 suggestCommands(bot);
