1ed63257e3b07f2107e47ca4b50945adf0426ee7
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-23 19:09:14 +0200

    fix: clean handling error

--- src/commands/clean.ts
+++ src/commands/clean.ts
@@ -2,6 +2,8 @@ import type { Api, Bot, Context, RawApi } from "grammy";
 
 export const cleanCommand = (bot: Bot<Context, Api<RawApi>>) => {
   bot.command("clean", async (ctx) => {
-    await ctx.deleteMessage();
+    await ctx.deleteMessage().catch((error) => {
+      console.error("Error caught:", error);
+    });
   });
 };
